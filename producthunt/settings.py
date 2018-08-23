76a40e694e43b62c18e43f110a65e7ca9f757471
Gareth Breeze <gareth.breeze1993@gmail.com>
2018-08-23 16:35:00 +0100

    Made a base.html

--- producthunt/settings.py
+++ producthunt/settings.py
@@ -123,5 +123,11 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'producthunt/static/')
+]
+# what directories django looks for the static files in. Including ones we've added.
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 STATIC_URL = '/static/'
