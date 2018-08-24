564aefb8a69b70dfe65bcab1d0a758768ac32afe
Gareth Breeze <gareth.breeze1993@gmail.com>
2018-08-24 19:17:56 +0100

    Created signup page

--- accounts/views.py
+++ accounts/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
+from django.contrib import auth
 
 def login(request):
     return render(request, 'accounts/login.html')
@@ -7,4 +9,18 @@ def logout(request):
     return render(request, 'accounts/signup.html')
 
 def signup(request):
-    return render(request, 'accounts/signup.html')	
+    if request.method == 'POST':
+        #User has entered info and wants an account
+        if request.POST['password1'] ==	request.POST['password2']:
+            try:		
+                user = User.objects.get(username=request.POST['username'])
+                return render(request, 'accounts/signup.html', {'error':'Username already exists'})		
+            except User.DoesNotExist:
+                user = User.objects.create_user(request.POST['username'], password = request.POST['password1'])
+                auth.login(request,user)
+                return redirect('home')	
+        else:
+            return render(request, 'accounts/signup.html', {'error':'Your passwords do not match'})		            		
+    else:
+        # User wants to enter info in order to sign up	
+        return render(request, 'accounts/signup.html')	
