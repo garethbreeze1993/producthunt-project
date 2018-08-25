6efe87e67001ed927515f2984aead28392aed035
Gareth Breeze <gareth.breeze1993@gmail.com>
2018-08-25 16:08:54 +0100

    Finished log in and log out functions udemy

--- accounts/views.py
+++ accounts/views.py
@@ -3,10 +3,21 @@ from django.contrib.auth.models import User
 from django.contrib import auth
 
 def login(request):
-    return render(request, 'accounts/login.html')
+    if request.method == 'POST':
+        user = auth.authenticate(username=request.POST['username'],password = request.POST['password']) # creates a user object if there is a valid user with that username and password in the system
+        if user is not None:
+            auth.login(request,user) # if a user object is created above login that person
+            return redirect('home')
+        else:
+            return render(request, 'accounts/login.html',{'error':'No matches for that Username and/or Password','user':user})		
+    else:
+        return render(request, 'accounts/login.html')
 	
 def logout(request):
-    return render(request, 'accounts/signup.html')
+    if request.method == 'POST':
+        auth.logout(request)# logs person out 
+        return redirect('home')		
+    
 
 def signup(request):
     if request.method == 'POST':
